6da9e0684b3e48b66439cccdadf399a5236e683e
Joel Klint <joel.klint@gmail.com>
2017-04-05 11:07:14 +0200

    Finished lab

--- sh.c
+++ sh.c
@@ -36,6 +36,9 @@ static list_t*	path_dir_list;		/* list of directories in PATH. */
 static int	input_fd;		/* for i/o redirection or pipe. */
 static int	output_fd;		/* for i/o redirection or pipe */
 
+char old_pwd[BUFSIZ];
+char next_old_pwd[BUFSIZ];
+
 /* fetch_line: read one line from user and put it in input_buf. */
 int fetch_line(char* prompt)
 {
@@ -162,68 +165,44 @@ void error(char *fmt, ...)
 void run_program(char** argv, int argc, bool foreground, bool doing_pipe)
 {
 	if(strcmp(argv[0], "cd") == 0) {
-		// HANDLE THIS CASE
-		// if(strcmp(argv[1], "-") == 0) {
-		// 	char* old_pwd = getenv("OLDPWD");
-		// 	printf("%s", old_pwd);
-		// 	setenv("OLDPWD", getenv("PWD"), 1);
-		// 	setenv("PWD", old_pwd, 1);
-		// 	chdir(old_pwd);
-		// }
-		// else {
-			setenv("OLDPWD", getenv("PWD"), 1);
+		if(argc == 1) {
+			getcwd(old_pwd, sizeof old_pwd);
+			chdir(getenv("HOME"));
+		}
+		else if(strcmp(argv[1], "-") == 0) {
+			getcwd(next_old_pwd, sizeof next_old_pwd);
+			chdir(old_pwd);
+			strcpy(old_pwd, next_old_pwd);
+		}
+		else {
+			// Could be fixed better to handle incorrect navigation
+			getcwd(old_pwd, sizeof old_pwd);
 			chdir(argv[1]);
-			setenv("PWD", argv[1], 1);
-		// }
+		}
 		return;
 	}
 
-	// int child_pid = fork();
 
 	// We are in the child process
 	int child_pid;
 	if((child_pid = fork()) == 0) {
-		// printf("Current command: %s\n", argv[0]);
-		// printf("%s pid: %d\n", argv[0], getpid());
-		// printf("Input pipe: %d\n", input_fd);
-		// printf("Output pipe: %d\n", output_fd);
 
 		if(input_fd != 0) {
-			// printf("pid %d changing input to: %d\n", getpid(), input_fd);
 			dup2(input_fd, 0);
 		}
 		if(output_fd != 0) {
-			// printf("pid %d changing output to: %d\n", getpid(), output_fd);
 			dup2(output_fd, 1);
 		}
-
-		// om input_fd != 0. dup2 input
-		// om output_fd != 0. dup2 output
 
 
 		// This only happens in case of Ampersand
 		if(!foreground && !doing_pipe) {
-			// printf("%s\n", "This is a background process");
 			output_fd = open("/dev/null", O_WRONLY);
 			dup2(output_fd, 1);
 		}
 
 		execvp(argv[0], argv);
 
-		// while(length(path_dir_list) != 0) {
-		// 	void* current_dir_path = remove_first(&path_dir_list);
-		// 	char program_path[100] = "";
-		// 	char *cur = program_path, * const end = program_path + sizeof program_path;
-		// 	cur += snprintf(cur, end-cur, "%s", current_dir_path);
-		// 	cur += snprintf(cur, end-cur, "%s", "/");
-		// 	cur += snprintf(cur, end-cur, "%s", argv[0]);
-		// 	if (access(program_path, F_OK|X_OK) == 0) {
-		// 		 int res = execv(program_path, argv);
-		// 		 if (res != 0) {
-		// 		 	printf("%s", "GOT ERROR WAT");
-		// 		 }
-		// 	}
-		// }
 
 		// Kill child process
 		printf("Could not find command\n");
@@ -232,12 +211,8 @@ void run_program(char** argv, int argc, bool foreground, bool doing_pipe)
 	}
 	else {
 		// Parent
-		// Man kan alltid vänta. Men kan bli bättre iom parallellt. Vänta på sista
-		// printf("\n");
 		if(!doing_pipe && foreground) {
-			// printf("Parent(%d) waiting for pid: %d\n", getpid(), child_pid);
-			int term_pid = waitpid(child_pid, NULL, 0);
-			// printf("Parent(%d) finished waiting for pid: %d\n", getpid(), term_pid);
+			waitpid(child_pid, NULL, 0);
 		}
 	}
 
@@ -280,12 +255,10 @@ void parse_line(void)
 
 		switch (type) {
 		case NORMAL:
-			// printf("NORMAL\n");
 			argc += 1;
 			break;
 
 		case INPUT:
-			// printf("INPUT\n");
 			type = gettoken(&argv[argc]);
 			if (type != NORMAL) {
 				error("expected file name: but found %s", 
@@ -301,7 +274,6 @@ void parse_line(void)
 			break;
 
 		case OUTPUT:
-			// printf("OUTPUT\n");
 			type = gettoken(&argv[argc]);
 			if (type != NORMAL) {
 				error("expected file name: but found %s", 
@@ -316,7 +288,6 @@ void parse_line(void)
 			break;
 
 		case PIPE:
-			// printf("PIPE\n");
 			doing_pipe = true;
 			pipe(pipe_fd);
 			output_fd = pipe_fd[1];
@@ -324,15 +295,12 @@ void parse_line(void)
 			/*FALLTHROUGH*/
 
 		case AMPERSAND:
-			// printf("AMPERSAND\n");
 			foreground = false;
 
 			/*FALLTHROUGH*/
 
 		case NEWLINE:
-			// printf("NEWLINE\n");
 		case SEMICOLON:
-			// printf("SEMICOLON\n");
 
 			if (argc == 0)
 				return;
@@ -425,7 +393,8 @@ static void init_search_path(void)
 int main(int argc, char** argv)
 {
 	progname = argv[0];
-
+	getcwd(old_pwd, sizeof old_pwd);
+	
 	init_search_path();	
 
 	while (fetch_line("% ") != EOF)
